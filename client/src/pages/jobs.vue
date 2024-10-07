5d3b8cd4bc849e41a20786fd8afb01e72e723449
Colin McDermott <colinm0125@gmail.com>
2024-10-07 15:11:35 -0400

    Did a lot

---
+++ client/src/pages/jobs.vue
@@ -0,0 +1,11 @@
+// eslint-disable-next-line vue/multi-word-component-names
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Jobs</h1>
+    <h2 class="subtitle">Find your dream job here</h2>
+    <p>We have a wide range of job opportunities available.</p>
+  </div>
+</template>
+<style scoped></style>
