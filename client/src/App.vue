37c40800bb2ff168757a593e1dad38e17c6ff5e2
Colin McDermott <colinm0125@gmail.com>
2024-10-07 14:25:42 -0400

    Some quick updates to the vite page. Fixed navbar a bit and made background better

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,26 @@
-<script setup lang="ts"> //setup indicates that we are using the composition API**
+<script setup lang="ts">
+//setup indicates that we are using the composition API**
 import { RouterView } from 'vue-router'
-import NavBar from './components/NavBar.vue';
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <header>
     <NavBar />
-
   </header>
 
-  <RouterView />
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
-
-
+<style scoped>
+body {
+  background-color: whitesmoke;
+}
+.container {
+  margin-top: 20px;
+  background-color: whitesmoke;
+  box-shadow: drop-shadow(0 0 5px rgba(0, 0, 0, 0.1));
+  min-height: 100vh;
+}
+</style>
